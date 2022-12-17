c8750bdcdf96bfbc18757b27cbef9053c15bf96a
Omer Šabić <omer@datalinx.si>
2022-12-17 10:48:18 +0100

    fix: fix typo

--- src/test.php
+++ src/test.php
@@ -5,4 +5,4 @@ echo PHP_EOL . PHP_EOL . 'Also, hello universe!';
 
 echo PHP_EOL . 'Fraticinida dexter genetrix est.';
 
-echo PHP_EOL . 'The searious pain of milk is to remember with control.';
+echo PHP_EOL . 'The serious pain of milk is to remember with control.';
