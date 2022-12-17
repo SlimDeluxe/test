87344434c2dd96a91b0bab490dc4c2733484dcb3
Omer Šabić <omer@datalinx.si>
2022-12-17 11:07:00 +0100

    feat: add culinary inspiration

--- src/test.php
+++ src/test.php
@@ -6,3 +6,5 @@ echo PHP_EOL . PHP_EOL . 'Also, hello universe!';
 echo PHP_EOL . 'Fraticinida dexter genetrix est.';
 
 echo PHP_EOL . 'The serious pain of milk is to remember with control.';
+
+echo PHP_EOL . 'With tofus drink cocktail saucee.';
